99f59032054e6e1685cba51f34676772013b8717
tkg-reis <oorgns1uw5sk3ae@gmail.com>
2024-06-22 14:20:52 +0900

    result.ejs output fix

--- public/js/main.js
+++ public/js/main.js
@@ -5,6 +5,15 @@ const val_1_N = document.querySelector(".val_1N").value;
 const val_1_NM = document.querySelector(".val_1NM").value;
 const val_2_N = document.querySelector(".val_2N").value;
 const val_2_NM = document.querySelector(".val_2NM").value;
+const output = document.querySelector("#output");
+
+if(Number(val_1_NM) > Number(val_2_NM)) {
+  output.innerHTML = `${val_1_N}が都会！！`
+} else if(Number(val_1_NM) < Number(val_2_NM)) {
+  output.innerHTML = `${val_2_N}が都会！！`
+} else {
+  output.innerHTML = "引き分け！"
+}
 
 new Chart(ctx, {
   type: "bar",
